03323a0ebf6a3496d1d8350194dbda24eaaacc5b
Florian Bocquet <niarolf@gmail.com>
2020-11-30 08:28:40 +0100

    Update script_refresh.sh
    utilisation d'une variable pour le chemin
    changement méthode en passant par le pipe pour le rappel et la suppression dès la commande fi,d

--- script_refresh.sh
+++ script_refresh.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
-z=""
-y=""
-y=""
-find /opt/mapproxy/cache_data/osm_cache_EPSG3857/ -name '*.png' -mtime 1 -fprint liste_png.txt
-for i in `cat liste_png.txt`
-  do
-    z=`echo $i | awk -F '/' '{print $6}'`
-    x=`echo $i | awk -F '/' '{print $7}'`
-    y=`echo $i | awk -F '/' '{print $8}'`
-    rm -f $i
-    curl -s "http://localhost/mapproxy/wmts/osm/webmercator/$z/$x/$y" >/dev/null
-  done
-rm -f liste_png.txt
+cache="/srv/cache_data/osm_cache_EPSG3857"
+echo "debut du refresh des tuiles du service wmts fondDePlan-OSM"
+find $cache -name '*.png' -type f -mtime +30 -delete -print | awk -F / '{print $(NF-2)"/"$(NF-1)"/"$NF}' |xargs -I % curl -s --url http://localhost/mapproxy/wmts/_fondDePlan-OSM/osm-grid/% >>/dev/null
+echo "fin du refresh des tuiles du service wmts _fondDePlan-OSM"
